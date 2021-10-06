fa3651676b78bde1239766357b5d0e55cb8b9284
Jose <santosjosel1995@gmail.com>
2021-10-06 16:42:49 -0400

    feat(): model for bugs and users

---
+++ models/bug.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bugSchema = new Schema({
+    bugName: { type: String, required: true, },
+    description: { type: String, required: false },
+    //status: Unassigned -> To Do -> In Progress -> QA -> Complete 
+    status: { type: String, required: true, },
+    //priority: low - med - high
+    priority: { type: String, required: true, },
+    reporter: { type: String, required: true, },
+    assginee: { type: String, required: false, },
+}, {
+    timestamps: true,
+});
+
+const Bug = mongoose.model('Bug', bugSchema);
+
+module.exports = Bug;
