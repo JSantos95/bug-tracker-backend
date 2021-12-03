4504f6bc06561b5b01d385bea31f9a21a9dbd0b9
Jose <santosjosel1995@gmail.com>
2021-12-03 15:21:49 -0500

    update(): func for create, join, delete company

--- controllers/company.ts
+++ controllers/company.ts
@@ -2,17 +2,41 @@ import { Request, Response } from "express";
 
 const Company = require('../models/Company');
 
+//Read
 export const find = (req: Request, res: Response) => {
     Company.find()
         .then((user: any) => res.json(user))
         .catch((err: any) => res.status(400).json('Error: ' + err));
 }
 
+//Create
 export const create = (req: Request, res: Response) => {
-    const companyname = req.body.companyname;
-    const newCompany = new Company({ companyname });
+    const newCompany = new Company({
+        companyName: req.body.companyName,
+        owner: req.body.username,
+        members: [req.body.username],
+    });
 
     newCompany.save()
-        .then(() => res.json('User added!'))
+        .then(() => res.json('Company added!'))
         .catch((err: any) => res.status(400).json('Error: ' + err));
 };
+
+//Update
+export const join = (req: Request, res: Response) => {
+    Company.findByIdAndUpdate(req.params.id)
+        .then((company: any) => {
+            company.members = [...company.members, req.body.username];
+
+            company.save()
+                .then(() => res.json('Company Updated!'))
+                .catch((err: any) => res.status(400).json('Error: ' + err));
+        })
+        .catch((err: any) => res.status(400).json('Error: ' + err));
+}
+
+export const deleteCompany = (req: Request, res: Response) => {
+    Company.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Bug deleted'))
+        .catch((err: any) => res.status(400).json('Error: ' + err));
+}
