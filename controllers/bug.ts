482a17342c2d0bf7b079843ac55394bbd14c3db8
Jose <santosjosel1995@gmail.com>
2022-08-02 14:39:56 -0400

    update(): to using ids instead of names feat(): get all bugs with jwt token

--- controllers/bug.ts
+++ controllers/bug.ts
@@ -1,43 +1,58 @@
 import { Request, Response } from "express";
-import { Bug as BugType } from '../interface';
+import { Bug as BugType, User as UserType } from '../interface';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
 const Bug = require('../models/Bug');
 
 //READ
 export const getAll = (req: Request, res: Response) => {
     Bug.find()
-        .then((bug: any) => res.json(bug))
+        .then((bug: BugType[]) => res.json(bug))
         .catch((err: any) => res.status(400).json('Error: lol ' + err));
 };
 
 export const getById = (req: Request, res: Response) => {
     Bug.findById(req.params.id)
-        .then((bug: any) => res.json(bug))
+        .then((bug: BugType) => res.json(bug))
         .catch((err: any) => res.status(400).json('Error: ' + err));
 };
 
 export const getUserBugs = (req: Request, res: Response) => {
     const name = req.params.name;
-    console.log(name);
     Bug.find({
         $or: [
-            { "reporter": name },
-            { "assginee": name },
+            { "reporterId": name },
+            { "assigneeId": name },
             { "status": "Unassigned" },
+            { "company": req.body.companyId ? req.body.companyId : "" }
+        ]
+    })
+        .then((bug: BugType[]) => res.json(bug))
+        .catch((err: any) => res.status(400).json('Error: ' + err));
+};
+
+export const getAllBugByToken = (req: Request, res: Response) => {
+    const { id } = jwt.decode(req.params.token) as JwtPayload;
+    Bug.find({
+        $or: [
+            { reporterId: id },
+            { assigneeId: id }
         ]
     })
         .then((bug: any) => res.json(bug))
-        .catch((err: any) => res.status(400).json('Error: ' + err));
+        .catch((err: any) => res.status(400).json('Error: ' + err))
 };
 
 //CREATE 
 export const create = (req: Request, res: Response) => {
     const bugName = req.body.bugName;
     const type = req.body.type;
-    const reporter = req.body.reporter;
+    const reporterId = req.body.reporterId;
     const description = req.body.description;
     const status = req.body.assginee ? 'To Do' : 'Unassigned';
     const priority = req.body.priority;
-    const assginee = req.body.assginee ? req.body.assginee : '';
+    const assigneeId = req.body.assigneeId ? req.body.assigneeId : '';
+    const companyId = req.body.companyId ? req.body.companyId : '';
 
     const newBug = new Bug({
         bugName,
@@ -45,8 +60,9 @@ export const create = (req: Request, res: Response) => {
         description,
         status,
         priority,
-        reporter,
-        assginee,
+        reporterId,
+        assigneeId,
+        companyId,
     })
 
     newBug.save()
@@ -56,14 +72,14 @@ export const create = (req: Request, res: Response) => {
 
 //UPDATE
 export const update = (req: Request, res: Response) => {
-    Bug.findByIdAndUpdate(req.params.id)
+    Bug.findByIdAndUpdate(req.body._id)
         .then((bug: BugType) => {
             bug.bugName = req.body.bugName;
             bug.type = req.body.type;
             bug.description = req.body.description;
             bug.status = req.body.status;
-            bug.reporter = req.body.reporter;
-            bug.assignee = req.body.assignee;
+            bug.reporterId = req.body.reporterId;
+            bug.assigneeId = req.body.assigneeId;
             bug.priority = req.body.priority;
 
             bug.save()
